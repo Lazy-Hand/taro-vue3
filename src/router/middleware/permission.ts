a854c9fa45b47c03d46f8455122d930d0e862b6b
wujiatong <17662073174@163.com>
2023-04-06 10:55:18 +0800

    feat: 🚀 router拦截规则

--- src/router/middleware/permission.ts
+++ src/router/middleware/permission.ts
@@ -1,15 +1,25 @@
-import { showModal } from '@tarojs/taro'
+import { showModal, showToast } from '@tarojs/taro'
 import Router, { registerMiddleware } from 'tarojs-router-next'
 import { useUserStore } from '@/store'
 registerMiddleware(async (_ctx, next) => {
 	const userStore = useUserStore()
-	if (!userStore.token) {
+	if (!userStore.token && _ctx.route.url !== '/pages/login/index') {
 		const { confirm } = await showModal({
 			title: '温馨提示',
 			content: '请先登录'
 		})
 		if (confirm) Router.toLogin()
 		return
+	} else {
+		if (_ctx.route.url === '/pages/login/index') {
+			showToast({
+				icon: 'error',
+				title: '请先退出登录',
+				mask: true,
+				duration: 3000
+			})
+			return
+		}
 	}
 
 	await next()
